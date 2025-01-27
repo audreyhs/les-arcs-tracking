a4d58c2971dad0917dae7dfe7cb07aaf62a9fb96
audreyhs <audrey@heriz-smith.com>
2025-01-27 20:34:38 +0000

    Update Supabase client configuration to use hardcoded URL and anon key

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-console.log(import.meta.env.VITE_SUPABASE_URL)
+const supabase_url = "https://svshfgivanaaeacxuclp.supabase.co";
+const supabase_anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2c2hmZ2l2YW5hYWVhY3h1Y2xwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3NDg3NDYsImV4cCI6MjA1MzMyNDc0Nn0.cXN3laQwQiNEK2VWLBlCJhpFq8g_iPQ_AwC6I3G9l3M";
 
 export const supabase = createClient(
-    process.env.supabase_url,
-    process.env.supabase_anon_key
+    supabase_url,
+    supabase_anon_key
   );
